fbd196a8648f22c86ff889f156c2cb1957dc971e
pas1317 <pasquario@gmail.com>
2024-03-18 21:36:13 -0300

    primer ejercicio

---
+++ pasquario.Carlos/primera clase/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace primera_clase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string apellido;
+            string edad;
+            Console.Write("Ingrese su nombre : ");
+            nombre = Console.ReadLine();
+            Console.Write("ingrese su apellido : ");
+            apellido = Console.ReadLine();
+            Console.Write(" ingrese su edad :  ");
+            edad = Console.ReadLine();
+            Console.WriteLine("bienvenido  " + nombre + "  " + apellido);
+            Console.WriteLine("  y su edad es : " + edad);
+                      
+        }
+    }
+}
